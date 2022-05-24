e106316a81753e54cb42acd064171913c49be3b3
Lim Jie Tymothy <tymlim96@gmail.com>
2022-05-24 20:39:45 +0800

    Update database uri to deal with deprecated "postgres://"

--- main.py
+++ main.py
@@ -18,7 +18,11 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+uri = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+# Workaround to replace start of DATABASE_URL with updated start
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
